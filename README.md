8533a0e1f26afee6e19daa27803a23194d39373d
James Hughes <jim.d.hughes@gmail.com>
2019-09-08 20:03:46 -0600

    adding some distinction to supported methods

---
+++ README.md
@@ -0,0 +1,5 @@
+# NASA API Proxy
+
+## Description
+
+A simple proxy with caching using BBolt for NASAs public API
--- router.go
+++ router.go
@@ -4,9 +4,9 @@ import "github.com/gorilla/mux"
 
 func NewRouter() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/neos/{date}", GetNeosByDate)
-	r.HandleFunc("/api/v1/marsweather", GetMarsWeather)
-	r.HandleFunc("/api/v1/apod", GetApod)
-	r.HandleFunc("/api/v1/apod/{date}", GetApod)
+	r.HandleFunc("/api/v1/neos/{date}", GetNeosByDate).Methods("GET")
+	r.HandleFunc("/api/v1/marsweather", GetMarsWeather).Methods("GET")
+	r.HandleFunc("/api/v1/apod", GetApod).Methods("GET")
+	r.HandleFunc("/api/v1/apod/{date}", GetApod).Methods("GET")
 	return r
 }
